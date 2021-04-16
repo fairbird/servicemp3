245271cc9081576c29797cb14486f025f8121940
Persian Prince <persianpros@yahoo.com>
2021-04-16 15:20:25 +0430

    Safe PEP8 (#18)
    * PEP8 double aggressive E251 and E252
    
    * PEP8 double aggressive E301 ~ E306
    
    * PEP8 double aggressive W291 ~ W293 and W391

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,9 +1,11 @@
 from Plugins.Plugin import PluginDescriptor
 
+
 def autostart(reason, **kwargs):
 	import servicemp3
 
+
 def Plugins(**kwargs):
 	return [
-		PluginDescriptor(where = PluginDescriptor.WHERE_AUTOSTART, needsRestart = True, fnc = autostart)
+		PluginDescriptor(where=PluginDescriptor.WHERE_AUTOSTART, needsRestart=True, fnc=autostart)
 	]

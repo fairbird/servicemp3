cb91565ee55f8321ff42939750a8638fc093dd92
Hains <hainsvdbosch@ziggo.nl>
2022-01-23 19:12:35 +0100

    Drop python 2

--- servicemp3/pythonmodule.cpp
+++ servicemp3/pythonmodule.cpp
@@ -1,12 +1,10 @@
 #include "servicemp3.h"
-#include <Python.h>
 
 static PyMethodDef servicemp3Methods[] =
 {
-	 {NULL,NULL,0,NULL}
+	{NULL,NULL,0,NULL}
 };
 
-#if PY_MAJOR_VERSION >= 3
 static struct PyModuleDef moduledef = {
 	PyModuleDef_HEAD_INIT,
 	"servicemp3",        /* m_name */
@@ -23,10 +21,3 @@ PyMODINIT_FUNC PyInit_servicemp3(void)
 {
 	return PyModule_Create(&moduledef);
 }
-#else
-PyMODINIT_FUNC
-initservicemp3(void)
-{
-	Py_InitModule("servicemp3", servicemp3Methods);
-}
-#endif
